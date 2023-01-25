49eec413924d54d2a07025b4a556288e975c4925
bismarck <bismarquemomanyi@gmail.com>
2023-01-24 23:35:12 -0500

    init
    first commit

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""a class defination square """
+
+class Square:
+    """ empty square """
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Square class definition"""
+
+
+class Square:
+    def __init__(self, size):
+        """ a constractor"""
+        self.__size = size
